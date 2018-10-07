57c6600cedaa9439efdb2f9306ac8a574ef0156d
jliriano <jd.liriano@alumnos.upm.es>
2018-10-07 11:45:35 +0200

    wrap up ReviewBuilder

--- src/main/java/apaw/ecp1/joel/liriano/ReviewBuilder.java
+++ src/main/java/apaw/ecp1/joel/liriano/ReviewBuilder.java
@@ -1,4 +1,36 @@
 package apaw.ecp1.joel.liriano;
 
 public class ReviewBuilder {
+
+    private Review review;
+
+    public ReviewBuilder ReviewBuilder(String reviewMessge) {
+        this.review = new Review (reviewMessge);
+        return this;
+    }
+
+    public ReviewBuilder title(String title) {
+        review.setTitle(title);
+        return this;
+    }
+
+    public ReviewBuilder author(String author) {
+        review.setAuthor(author);
+        return this;
+    }
+
+    public ReviewBuilder rating(int rating) {
+        review.setReviewRating(rating);
+        return this;
+    }
+
+    public ReviewBuilder pendingApproval(boolean pending) {
+        review.setPendingApproval(pending);
+        return this;
+    }
+
+    public Review build() {
+        return review;
+    }
+
 }
